1d95b60a276c3ba468ffa3ba81911ac41aa6151d
Xseis <114229348+Casper6ybaka@users.noreply.github.com>
2025-05-09 10:56:58 +0200

    hotfix

--- DatabasenWebApp/Controllers/EditBioController.cs
+++ DatabasenWebApp/Controllers/EditBioController.cs
@@ -9,24 +9,7 @@ namespace DatabasenWebApp.Controllers
     [Route("api/EditBio")]
     public class EditBioController : Controller
     {
-        private readonly ILogger<EditBioController> _logger;
         GetUserData getUserData = new GetUserData();
-
-        public EditBioController(ILogger<EditBioController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
 
         [HttpPost("edit-bio")]
         public IActionResult editBio([FromBody] editBioClass data)
